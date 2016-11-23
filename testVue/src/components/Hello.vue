9935a08de6297a6836b645558c54ce290ba42004
cai <1085542424@qq.com>
2016-11-23 17:35:30 +0800

    画图组件

--- testVue/src/components/Hello.vue
+++ testVue/src/components/Hello.vue
@@ -1,31 +1,55 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-      <br>
-      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+    <span>高度: </span><input type="text" v-model.lazy="heightInfo" >px
+    <span>宽度: </span><input type="text" v-model.lazy="widthInfo">px
+
+    <chartjs-line :labels="labels" :data="labelData" :width="width" :height="height" :beginzero="beginzero" :datalabel="datalabel" :linetension="linetension" v-if="upDate"></chartjs-line>
   </div>
 </template>
 
 <script>
+const labelDataTable = () => { return [20, 60, 40, 50, 500] }
+const labels = () => { return [5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60] }
 export default {
   name: 'hello',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      upDate: true,
+      datalabel: '在线人数',
+      beginzero: false,
+      labels: labels(),
+      labelData: labelDataTable(),
+      width: 500,
+      height: 500,
+      heightInfo: '',
+      widthInfo: '',
+      linetension: 0.2
+    }
+  },
+  watch: {
+    heightInfo () {
+      console.log(' height is changed  11111111')
+      let heightInfo = Number(this.heightInfo)
+      if (heightInfo && heightInfo > 0) {
+        console.log(' height is changed 22222222')
+        this.upDate = false
+        this.height = heightInfo
+        this.$nextTick(() => {
+          this.upDate = true
+        })
+      }
+    },
+    widthInfo () {
+      console.log(' widthInfo is changed  11111111')
+      let widthInfo = Number(this.widthInfo)
+      if (widthInfo && widthInfo > 0) {
+        console.log(' widthInfo is changed 22222222')
+        this.upDate = false
+        this.width = widthInfo
+        this.$nextTick(() => {
+          this.upDate = true
+        })
+      }
     }
   }
 }
@@ -33,21 +57,4 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
 </style>
--- testVue/src/main.js
+++ testVue/src/main.js
@@ -1,7 +1,16 @@
 import Vue from 'vue'
 import App from './App'
-
+import 'chart.js'
+import 'hchs-vue-charts'
+Vue.use(window.VueCharts)
 /* eslint-disable no-new */
+window.onerror = function (err, errUrl, errLine) {
+  console.error('error =>' + err + '\nerrUrl => ' + errUrl + '\nerrLine => ' + errLine)
+}
+Vue.config.errorHandler = function (err, vm) {
+  //  致命错误
+  console.error('componentName: ' + vm.$data.componentName + ' error => ', err)
+}
 new Vue({
   el: '#app',
   template: '<App/>',
